ae4aa3834471f10c668490939c5eacbee775cf28
Wolf-Cat <961776021@qq.com>
2023-05-27 15:35:21 +0800

    工程搭建

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 extern "C" {
-    //#include <libavutil/log.h>
+    #include <libavutil/log.h>
+    #include <libavformat/avformat.h>
     //#include <SDL.h>
 }
 
@@ -12,14 +13,13 @@ using namespace std;
 int main(int argc, char *agrv[])
 {
     char fileAvPath[10] = "./mp4";
-    int a = 0;
-    a = 1;
-    int k  = 8999;
     std::cout << "hello wiord" << std::endl;
-    //av_log_set_level(AV_LOG_DEBUG);
-    //av_log(NULL, AV_LOG_INFO, "Hello ffmpeg");
-    std::cout << "hello wiordGFFFFFFFFddddddd" << std::endl;
-    
+    av_log_set_level(AV_LOG_DEBUG);
+    av_log(NULL, AV_LOG_INFO, "Hello ffmpeg");
+
+    uint32_t version = avformat_version();
+    cout << "version:" << version << endl;
+
     //if(SDL_Init(SDL_INIT_EVERYTHING) == 0)
     {
         std::cout << "SDL init success" << std::endl;
