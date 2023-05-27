17715eb187cf0a8c686a051bf5bd58f2fd535a3e
Wolf-Cat <961776021@qq.com>
2023-05-27 21:55:53 +0800

    创建SDL窗口和Renderer

--- main.cpp
+++ main.cpp
@@ -7,23 +7,50 @@ extern "C" {
 
 using namespace std;
 
-SDL_Window *g_pWin = NULL;
-SDL_Renderer *g_pRenderer = NULL;
+namespace {
+    const int WIN_WIDTH = 640;
+    const int WIN_HEIGHT = 480;
+}
+
+static SDL_Window *g_pWindow = NULL;
+static SDL_Renderer *g_pRenderer = NULL;
 
 int main(int argc, char *agrv[])
 {
-    char fileAvPath[10] = "./mp4";
-    std::cout << "hello wiord" << std::endl;
     av_log_set_level(AV_LOG_DEBUG);
-    av_log(NULL, AV_LOG_INFO, "Hello ffmpeg");
+    av_log(NULL, AV_LOG_INFO, "Hello ffmpeg\n");
 
     uint32_t version = avformat_version();
     cout << "version:" << version << endl;
 
-    if(SDL_Init(SDL_INIT_EVERYTHING) == 0)
+    SDL_Texture *pTextTure = NULL;
+    char fileAvPath[10] = "./mp4";
+
+    if(SDL_Init(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == 0))
     {
         std::cout << "SDL init success" << std::endl;
     }
+
+    g_pWindow = SDL_CreateWindow("Cat Audio Video",
+                                 SDL_WINDOWPOS_UNDEFINED,
+                                 SDL_WINDOWPOS_UNDEFINED,
+                                 WIN_WIDTH, WIN_HEIGHT,
+                                 SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    if(g_pWindow == NULL)
+    {
+        fprintf(stderr, "Create window failed, %s\n", SDL_GetError());
+        goto __END;
+    }
+
+    g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+    if(g_pRenderer == NULL)
+    {
+        fprintf(stderr, "Create Renderer failed, %s\n", SDL_GetError());
+        goto __END;
+    }
+
+    __END:
 
     return 0;
 }
