237496ea9238f941a936b975dd13bcbf1c5029da
Wolf-Cat <961776021@qq.com>
2023-05-29 00:14:25 +0800

    完成音视频包队列的初始化函数

--- AVPacketQueue.cpp
+++ AVPacketQueue.cpp
@@ -1,1 +1,25 @@
 #include "AVPacketQueue.h"
+
+int AVPacketQueue::InitPacketQueue()
+{
+    m_pPacketList = av_fifo_alloc2(1, sizeof(MyAVPacketList), AV_FIFO_FLAG_AUTO_GROW);
+    if(NULL == m_pPacketList)
+    {
+        return -1;
+    }
+
+    m_pMutex = SDL_CreateMutex();
+    if(NULL == m_pMutex)
+    {
+        av_log(NULL, AV_LOG_FATAL, "SDL_CreateMutex():%s\n", SDL_GetError());
+        return -2;
+    }
+
+    m_pCond = SDL_CreateCond();
+    if(NULL == m_pCond)
+    {
+        av_log(NULL, AV_LOG_FATAL, "SDL_CreateCond():%s\n", SDL_GetError());
+        return -3;
+    }
+    return 0;
+}
--- AVPacketQueue.h
+++ AVPacketQueue.h
@@ -2,14 +2,22 @@
 #define AUDIOVIDEOPLAYER_AVPACKETQUEUE_H
 
 extern "C" {
+#include <libavutil/fifo.h>
+#include <libavcodec/packet.h>
+#include <libavutil/log.h>
 #include <SDL2/SDL_mutex.h>
 };
 
+typedef struct MyAVPacketList {
+    AVPacket *pkt;
+} MyAVPacketList;
+
 class AVPacketQueue {
 public:
-    void InitPacketQueue();
+    int InitPacketQueue();
 
 private:
+    AVFifo *m_pPacketList = NULL;
     int m_nCountPackets = 0;
     int m_nQueueSize = 0;
     int64_t m_duration = 0;
